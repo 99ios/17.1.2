26a2627a47a56ba0911c0a72cb876bb327e6e421
test <422538722@qq.com>
2017-04-16 16:53:13 +0800

    17.1.2
    获取电池信息batteryState

---
+++ 17.1.2 获取电池信息batteryState/17.1.2 获取电池信息batteryState/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  17.1.2 获取电池信息batteryState
+//
+//  Created by 李维佳 on 2017/4/7.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
